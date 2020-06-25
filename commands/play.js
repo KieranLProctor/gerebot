0593f4c605f6233a2b9b9ad7130f1a4e3f72438c
Kieran Proctor <kieranproctor@kierans-mbp.home>
2020-06-25 13:16:46 +0100

    Changed play command to be a promise

--- commands/play.js
+++ commands/play.js
@@ -4,53 +4,55 @@ const config = require('../config.json');
 const moment = require('moment');
 
 module.exports = {
-  name: 'play',
-  description: 'Plays Youtube videos to the voice channel you are connected to.',
-  aliases: ['pyt', 'pm', 'p'],
-  usage: '<query|link>',
-  args: true,
-  async execute(client, message, args) {
-    const embed = new Discord.MessageEmbed();
-    const guildID = message.guild.id;
-    const toPlay = args.toString().replace(/,/g, ' ');
+    name: 'play',
+    description: 'Plays Youtube videos to the voice channel you are connected to.',
+    aliases: ['pyt', 'pm', 'p'],
+    usage: '<query|link>',
+    args: true,
+    async execute(client, message, args) {
+        const embed = new Discord.MessageEmbed();
+        const guildID = message.guild.id;
+        const toPlay = args.toString().replace(/,/g, ' ');
 
-    let userVoiceChannel = message.member.voice.channel;
-    if (!userVoiceChannel) return message.reply('❌ You must be in a voice channel before I can play!');
+        let userVoiceChannel = message.member.voice.channel;
+        if (!userVoiceChannel) return message.reply('❌ You must be in a voice channel before I can play!');
 
-    // If already playing => add song to the queue.
-    let isPlaying = client.player.isPlaying(guildID);
-    if (isPlaying) {
-      let song = await client.player.addToQueue(guildID, toPlay, message.member.user.tag);
+        // If already playing => add song to the queue.
+        let isPlaying = client.player.isPlaying(guildID);
+        if (isPlaying) {
+            let song = await client.player.addToQueue(guildID, toPlay, message.member.user.tag);
 
-      // embed.setAuthor(message.author.username)
-      //   .setColor(config.colors.embed)
-      //   .setDescription(`[${track.name}](${track.url})`)
-      //   .addField('¬ Playing in', voiceChannel.name, true)
-      //   .addFields(
-      //     { name: '¬ Duration', value: moment(track.duration).format('HH:mm:ss'), inline: true },
-      //     { name: '¬ Channel', value: track.author, inline: true },
-      //     { name: '¬ Requester', value: track.requestedBy, inline: true }
-      //   )
-      //   .addField('¬ Users', 1, true)
-      //   .setTimestamp();
+            // embed.setAuthor(message.author.username)
+            //   .setColor(config.colors.embed)
+            //   .setDescription(`[${track.name}](${track.url})`)
+            //   .addField('¬ Playing in', voiceChannel.name, true)
+            //   .addFields(
+            //     { name: '¬ Duration', value: moment(track.duration).format('HH:mm:ss'), inline: true },
+            //     { name: '¬ Channel', value: track.author, inline: true },
+            //     { name: '¬ Requester', value: track.requestedBy, inline: true }
+            //   )
+            //   .addField('¬ Users', 1, true)
+            //   .setTimestamp();
 
-      message.channel.send('added to queue');
-    } else {
-      let song = await client.player.play(userVoiceChannel, toPlay, message.member.user.tag);
-
-      message.channel.send('playing');
-
-      song.queue.on('end', () => {
-        message.channel.send('finished');
-      });
-
-      song.queue.on('songChanged', (oldSong, newSong, skipped, repeatMode) => {
-        if (repeatMode) {
-          message.channel.send('repeat');
+            message.channel.send('added to queue');
         } else {
-          message.channel.send('play');
+            client.player.play(userVoiceChannel, toPlay, message.member.user.tag).then(res => {
+                message.channel.send('playing');
+
+                song.queue.on('end', () => {
+                    message.channel.send('finished');
+                });
+
+                song.queue.on('songChanged', (oldSong, newSong, skipped, repeatMode) => {
+                    if (repeatMode) {
+                        message.channel.send('repeat');
+                    } else {
+                        message.channel.send('play');
+                    }
+                });
+            }).catch(errpr => {
+                return message.reply(`Couldn't find any results for ${args}`);
+            });
         }
-      });
     }
-  }
 };
